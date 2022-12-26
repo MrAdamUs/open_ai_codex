f2fb93d5e0d7ea3e4ca4eb386a3acf332b97a753
Adam Mawlawi <adam@mradam.us>
2022-12-25 18:53:51 -0600

    start the project using vitejs and start writing the js functions

---
+++ client/script.js
@@ -0,0 +1,31 @@
+import bot from "./assets/bot.svg"
+import user from "./assets/user.svg"
+
+const form = document.querySelector("form")
+const chatContainer = document.querySelector("#chat_container")
+
+let loadInterval
+
+function loader(element) {
+  element.textContent = ""
+
+  loadInterval = setInterval(() => {
+    element.textContent += "."
+
+    if (element.textContent === "....") {
+      element.textContent = ""
+    }
+  }, 300)
+}
+
+function typeText(ele, text) {
+  let index = 0
+
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      ele.innerHTML += text.chartAt(index)
+    } else {
+      clearInterval(interval)
+    }
+  }, 20)
+}
