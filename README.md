dd9eae44d0bdb980df2cb263da12e65a4e0aa364
Adam Mawlawi <adam@mradam.us>
2022-12-25 22:00:20 -0600

    done

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # open_ai_codex
+
+Install `npm i cors dotenv express nodemon openai ` for the server
--- client/script.js
+++ client/script.js
@@ -83,6 +83,33 @@ const handleSubmit = async (e) => {
 
   // messageDiv.innerHTML = "..."
   loader(messageDiv)
+
+  // fetch data from server
+
+  const response = await fetch("http://localhost:5500", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt: data.get("prompt"),
+    }),
+  })
+
+  clearInterval(loadInterval)
+  messageDiv.innerHTML = ""
+
+  if (response.ok) {
+    const data = await response.json()
+    const parsedData = data.bot.trim()
+
+    typeText(messageDiv, parsedData)
+  } else {
+    const err = await response.text()
+    messageDiv.innerHTML = "Something went wrong "
+
+    alert(err)
+  }
 }
 
 form.addEventListener("submit", handleSubmit)
